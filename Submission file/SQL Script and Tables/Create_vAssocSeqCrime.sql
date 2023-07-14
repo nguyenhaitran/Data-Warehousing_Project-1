de0848045e5726a4be4c8b93d9e7f8419eab9f87
nguyenhaitran <126839412+nguyenhaitran@users.noreply.github.com>
2023-07-14 22:28:42 +0800

    Delete Submission file directory

--- Submission file/SQL Script and Tables/Create_vAssocSeqCrime.sql
+++
@@ -1,8 +0,0 @@
-USE CrimeDW;
-GO
-CREATE VIEW [dbo].[vAssocSeqCrime]
-AS
-SELECT DISTINCT Fact.Crime_ID, Crime.crime
-FROM [dbo].[FactTable] AS Fact 
-JOIN [dbo].[DimCrime] AS Crime ON Fact.Crime_ID = Crime.ID 
-GO
